3f79032064c1e98e28a57279ca6dd90e7eb3a2ed
Andrew Ryouta <A.Ryouta@hotmail.com>
2017-10-30 19:51:48 +0200

    no message

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int n = 1, i = 0;
+            Console.ForegroundColor = ConsoleColor.Red;
+            /*
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+                n++;
+            }
+            */
+            
+            do
+            {
+                Console.Write("*");
+                i++;
+                if (i == n)
+                {
+                    n++;
+                    i = 0;
+                    Console.Write("\n");
+                }
+            }
+            while (n <= 5);
+
             Console.BackgroundColor = ConsoleColor.Green;            
             Console.WriteLine("Hi!");
             Console.ReadLine();
