b2a0d2f7ac16a4a5c0d38af620315c55c32afbc8
Andrew Ryouta <A.Ryouta@hotmail.com>
2017-11-02 16:14:24 +0200

    Homework #5 - shapes

---
+++ CSharpHW/5/DrawFigures/DrawFigures/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                Console.WriteLine("Select 2DShape Triangle - g, Square - s, Romb - r.\nTo exit press q");
+                string shape = Console.ReadLine();
+
+                if(shape == "q")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Select your 2DShape size from 1 to 10");
+                int size = int.Parse(Console.ReadLine());
+                                
+                StringBuilder block = new StringBuilder("");
+                
+                switch (shape)
+                {
+                    case "g":
+                        for(int i = 0; i < size; i++)
+                        {
+                            block.Append("*");
+                            Console.WriteLine(block);
+                        }
+                        break;
+                    case "s":
+                        block.Append('*', size);
+                        for(int i = 0; i < size; i++)
+                        {
+                            Console.WriteLine(block);
+                        }
+                        break;
+                    case "r":
+                        StringBuilder space = new StringBuilder();
+                        space.Append(' ', size);
+                        block.Append("*");
+
+                        for(int i = 0; i < size; i++)
+                        {
+                            if (i < (size / 2))
+                            {
+                                Console.WriteLine("{0}{1}", space, block);
+                                space.Remove(0, 1);
+                                block.Append("**");
+                            }
+                            else if (i > (size / 2))
+                            {
+                                if (i <= size - 1)
+                                {
+                                    block.Remove(0, 2);
+                                }
+                                else
+                                {
+                                    block.Remove(0, 1);
+                                }
+                                space.Append(" ");
+                                Console.WriteLine("{0}{1}", space, block);                                
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0}{1}", space, block);
+                            }
+                        }
+                        break;
+                    case "q":
+                        exit = true;
+                        break;
+                }                
+            } while (!exit);
+        }
+    }
+}
