759d935f2aaf8685621c8c2517273b2e27b8b4a8
Andrew Ryouta <A.Ryouta@hotmail.com>
2017-11-10 01:25:54 +0200

    Homework #8

---
+++ CSharpHW/8/TypeOperations/CarFabric/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFabric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var china = new Engine("China", 400);
+            var maibah = new Engine("Maibah", 1400);
+
+            var red = new Color("Red");
+            var black = new Color("Black");
+
+            var soft = new Transmission("Soft");
+            var hard = new Transmission("Hard");
+
+            Car mersedes = CarConstructor.Construct(maibah, black, soft);
+            mersedes.Name = "Mersedes";
+            mersedes.Print();
+
+            Car mitsubishi = CarConstructor.Construct(china, red, hard);
+            mitsubishi.Name = "Mitsubishi";
+            mitsubishi.Print();
+
+            CarConstructor.Reconstruct(mitsubishi);
+            mitsubishi.Print();
+
+            Console.ReadLine();
+        }
+    }
+    internal class Car
+    {
+        public string Name { get; set; }
+        public string EngineMark { get; set; }
+        public int EngineCapacity { get; set; }
+        public string Color { get; set; }
+        public string TransmissionType { get; set; }
+        public Car()
+        { }
+        public Car(string _name)
+        {
+            Name = _name;
+        }
+        public void Print()
+        {
+            Console.WriteLine("{0} {1} has {2} engine with {3} power and {4} transmission",
+                Color, Name, EngineMark, EngineCapacity, TransmissionType);
+        }
+    }
+    internal static class CarConstructor
+    {
+        public static Car Construct(Engine e, Color c, Transmission tr)
+        {
+            var current = new Car();            
+            current.EngineMark = e.Mark;
+            current.EngineCapacity = e.Capacity;
+            current.Color = c.ColorName;
+            current.TransmissionType = tr.Type;
+            return current;
+        }
+        public static void Reconstruct(Car c)
+        {
+            c.EngineMark = "Yamaha";
+            c.EngineCapacity = 800;
+        }
+    }
+    internal class Engine
+    {
+        public string Mark { get; set; }
+        public int Capacity { get; set; }
+        public Engine(string _mark, int _cap)
+        {
+            Mark = _mark;
+            Capacity = _cap;
+        }
+    }
+    internal class Color
+    {
+        public string ColorName { get; set; }
+        public Color(string _color)
+        {
+            ColorName = _color;
+        }
+    }
+    internal class Transmission
+    {
+        public string Type { get; set; }
+        public Transmission(string _type)
+        {
+            Type = _type;
+        }
+    }    
+
+}
---
+++ CSharpHW/8/TypeOperations/TypeOperations/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var something = 5;
+            var copy = something;
+
+            PrintSomething(something, copy);            
+            copy += 5;
+            PrintSomething(something, copy);
+
+            var jerry = new User
+            {
+                FirstName = "Jerry",
+                LastName = "Torvald",
+                Age = 21
+            };
+            var tom = jerry;
+
+            jerry.Print();
+            tom.Print();
+            Console.WriteLine();
+
+            tom.Age = 30;
+
+            jerry.Print();
+            tom.Print();
+            Console.WriteLine();
+
+            var anna = new User();
+            anna.Clone(jerry);
+
+            jerry.Print();
+            anna.Print();
+            Console.WriteLine();
+
+            anna.FirstName = "Anna";
+            anna.Age = 19;
+
+            jerry.Print();
+            anna.Print();
+            Console.ReadLine();
+        }        
+
+        static void PrintSomething(params int[] args)
+        {
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", i, args[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+    class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public void Clone(User value)
+        {
+            this.FirstName = value.FirstName;
+            this.LastName = value.LastName;
+            this.Age = value.Age;
+        }
+        public void Print()
+        {
+            Console.WriteLine("{0} {1} is {2} years old", FirstName, LastName, Age);
+        }              
+    }
+}
