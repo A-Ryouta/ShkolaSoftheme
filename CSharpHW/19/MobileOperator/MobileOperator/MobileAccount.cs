1aeef9ec3413ff87529f9f1489a2aac3e87750fa
Andrew Ryouta <A.Ryouta@hotmail.com>
2017-12-06 15:58:34 +0200

    Homework #19

---
+++ CSharpHW/19/MobileOperator/MobileOperator/MobileAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MobileOperator
+{
+    internal class MobileAccount
+    {
+        public delegate void AccountActionsHandler(MobileAccount sender, ActionEventArgs e);
+        public event EventHandler<ActionEventArgs> Message;
+        public event EventHandler<ActionEventArgs> Call;
+
+        public int Number { get; }
+        public Dictionary<int, string> Contacts { get; }
+
+        public MobileAccount(int number)
+        {
+            Number = number;
+            Contacts = new Dictionary<int, string>();
+        }
+
+        public void AddContact(int number, string name)
+        {
+            Contacts.Add(number, name);
+        }
+
+        public void SendMessage(int receiver, string message)
+        {
+            Message?.Invoke(this, new ActionEventArgs(receiver, message));
+        }
+
+        public void TryToCall(int receiver)
+        {
+            Call?.Invoke(this, new ActionEventArgs(receiver));
+        }
+
+        public void TakeMessage(int senderNumber, string message)
+        {
+            if (Contacts.Any(u => u.Key == senderNumber))
+            {
+                Console.WriteLine("Take message from {0} : {1}", Contacts[senderNumber], message);
+            }
+            else
+            {
+                Console.WriteLine("Take message from {0} : {1}", senderNumber, message);
+            }
+
+        }
+
+        public void TakeCall(int senderNumber)
+        {
+            if (Contacts.Any(u => u.Key == senderNumber))
+            {
+                Console.WriteLine("Incoming call from {0}", Contacts[senderNumber]);
+            }
+            else
+            {
+                Console.WriteLine("Incoming call from {0}", senderNumber);
+            }
+
+        }
+    }
+}
---
+++ CSharpHW/19/MobileOperator/MobileOperator/OperatorEventArgs.cs
@@ -0,0 +1,20 @@
+﻿namespace MobileOperator
+{
+    internal class ActionEventArgs
+    {
+        public string Message { get; }
+        public int Receiver { get; }
+
+        public ActionEventArgs(int receiver)
+        {
+            Message = string.Empty;
+            Receiver = receiver;
+        }
+
+        public ActionEventArgs(int receiver, string message)
+        {
+            Message = message;
+            Receiver = receiver;
+        }
+    }
+}
